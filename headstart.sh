0492804af91bf9cbc0d3530da715ed4f2c437796
eXit-g <62702100+eXit-g@users.noreply.github.com>
2020-03-26 05:43:40 -0700

    Add files via upload

---
+++ headstart.sh
@@ -0,0 +1,68 @@
+cd Desktop
+PURPLE='\033[1;35m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+CY='\033[0;36m'
+NC='\033[0m' # No Color
+echo -e "Welcome to ${YELLOW}eXit's${NC} Kali Linux tool installer! The installation will begin."
+sleep 2
+echo -e "Installing ${PURPLE}Manisso's${CY} fsociety${NC} toolkit!"
+sleep 2
+git clone https://github.com/Manisso/fsociety
+sleep 2
+echo -e "${CY}fsociety${NC} tool succesfully installed!"
+sleep 2
+echo -e "Installing ${PURPLE}Cyb0r9's${CY} SocialBox${NC} tool!"
+sleep 2
+git clone https://github.com/Cyb0r9/SocialBox
+echo -e "${CY}SocialBox${NC} tool succesfully installed!"
+sleep 2
+echo -e "Installing ${PURPLE}thelinuxchoice's${CY} saycheese${NC} tool!"
+sleep 2
+git clone https://github.com/thelinuxchoice/saycheese
+echo -e "${CY}saycheese${NC} tool succesfully installed!"
+sleep 2
+echo -e "Installing ${PURPLE}mebus's${CY} cupp${NC} tool!"
+sleep 2
+git clone https://github.com/Mebus/cupp
+echo -e "${CY}cupp${NC} tool succesfully installed!"
+sleep 2
+echo -e "Installing ${PURPLE}rand0m1ze's${CY} ezsploit${NC} tool!"
+sleep 2
+git clone https://github.com/rand0m1ze/ezsploit
+echo -e "${CY}ezsploit{NC} tool succesfully installed!"
+sleep 2
+echo -e "Installing ${PURPLE}Hackplayers's${CY} 4nonimizer${NC} tool!"
+sleep 2
+git clone https://github.com/Hackplayers/4nonimizer
+echo -e "${CY}4nonimizer${NC} tool succesfully installed!"
+sleep 3
+echo -e "Installing ${PURPLE}arismelachroinos's${CY} lscript${NC} tool!"
+sleep 2
+git clone https://github.com/arismelachroinos/lscript
+echo -e "The ${CY}Lazy Script${NC} is succesfully installed!"
+sleep 3
+echo -e "Installing ${PURPLE}UndeadSec's${CY} SocialFish${NC} tool!"
+sleep 2
+https://github.com/UndeadSec/SocialFish
+echo -e "${CY}SocialFish ${NC}tool is succesfully installed!"
+sleep 2
+echo -e "Installing ${RED}armitage${NC}!"
+sleep 2
+sudo apt-get install armitage
+echo -e "${RED}armitage ${NC}succesfully installed!"
+sleep 2
+echo -e "Installing ${RED}metasploit${NC}!"
+sleep 2
+sudo apt-get install metasploit
+echo -e "${RED}metasploit ${NC}succesfully installed!"
+sleep 3
+echo -e "Installing the ${RED}Tor Browser${NC}!"
+sleep 2
+sudo apt-get install torbrowser
+echo -e "${RED}Tor Browser${NC} succesfully installed!"
+echo -e "${RED}Thanks for using my script!"
+echo -e "Installer tool by: ${YELLOW}eXit${NC} Good luck to everyone!"
+printf "Kind ${RED}regards${NC} to all pentester mates!\n"
+sleep 3
+
